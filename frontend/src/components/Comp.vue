07670c8de348279304bfe297c592e6b80ef8fcc6
Juan <jonhy.simaj@gmail.com>
2020-11-21 02:38:33 -0600

    agregando el vue

---
+++ frontend/src/components/Comp.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div>
+
+    <p>Hola imprime ahora!!</p>
+
+  </div>
+
+</template>
+
+<script>
+  export default{
+    name: 'CompanyData',
+    data(){
+      return {}
+    }
+  }
+</script>
+
+<style scoped>
+</style>
