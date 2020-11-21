882b5deb350929c4bd86c344bb89caead3f6ca17
Juan <jonhy.simaj@gmail.com>
2020-11-21 16:44:44 -0600

    agregando bootstrapt y axios

--- frontend/src/components/Comp.vue
+++ frontend/src/components/Comp.vue
@@ -2,17 +2,46 @@
 
   <div>
 
-    <p>Hola imprime ahora!!</p>
+    <p>Hola mundo </p>
+    {{ directory }}
 
   </div>
 
 </template>
 
 <script>
+  import axios from 'axios'
   export default{
-    name: 'CompanyData',
+  	name: 'CompanyData',
     data(){
-      return {}
+      return {
+      	directory: []
+      }
+    },
+    async mounted () {
+    	try {
+    		var result = await axios({
+    			method: 'POST',
+    			url: 'http://127.0.0.1:8000/graphql/',
+    			data: {
+    				query: `
+    				{
+					  allTitles {
+					    edges {
+					      node {
+					        id
+					        titleName
+					      }
+					    }
+					  }
+					}
+    				`
+    			}
+    		})
+    		this.directory = result.data.data.allTitles
+    	} catch (error) {
+    		console.error(error)
+    	}
     }
   }
 </script>
